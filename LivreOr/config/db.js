ccff829dd5af7ca70893b7c3d8d32f3eeb17384a
ErwanVerzat <verzat_erwan@hotmail.fr>
2021-09-10 16:45:03 +0200

    Add Livredor

---
+++ LivreOr/config/db.js
@@ -0,0 +1,9 @@
+let mysql      = require('mysql');
+let connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '1234',
+    database : 'my_schema'
+});
+
+connection.connect();
---
+++ LivreOr/server.js
@@ -0,0 +1,45 @@
+let express = require('express')
+let app = express()
+let bodyParser = require('body-parser')
+let session = require('express-session')
+
+// Moteur de template
+app.set('view engine', 'ejs')
+
+//MiddleWare
+app.use('/assets', express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+app.use(session({
+  secret: 'secretkey',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
+app.use(require('./middleware/flash'))
+
+// Routes
+app.get('/', (request, response) => {
+    console.log(request.session)
+    response.render('pages/index', {test: 'Salut'})
+})
+
+app.post('/', (request,response) => {
+    if (request.body.message ===undefined || request.body.message === ''){
+
+        request.flash('error', "Vous n'avez pas posté de message")
+
+
+    }else{
+        let Message = require('./models/message')
+        Message.creat(request.body.message, function (){
+            request.flash('succes', "Merci !")
+        })
+    }
+    response.redirect('/')
+})
+
+app.listen(8080)
+console.log("reload2")
