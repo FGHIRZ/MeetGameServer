0ae5abf12a62d487ceeff820050fdadb45af4708
ErwanVerzat <verzat_erwan@hotmail.fr>
2021-09-11 12:09:46 +0200

    update livredor

--- LivreOr/config/db.js
+++ LivreOr/config/db.js
@@ -1,6 +1,12 @@
 let mysql = require('mysql');
 
-m
+let connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    port     : 3306,
+    password : '1234',
+    database : 'my_schema'
+});
 
 connection.connect();
 
