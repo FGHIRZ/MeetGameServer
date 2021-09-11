be751813d035ae48d3e169d49e5d5e083128fac2
ErwanVerzat <verzat_erwan@hotmail.fr>
2021-09-11 12:58:40 +0200

    update livredor

--- LivreOr/server.js
+++ LivreOr/server.js
@@ -28,13 +28,14 @@ app.get('/', (request, response) => {
 })
 
 app.post('/', (request,response) => {
-    if (request.body.message ===undefined || request.body.message === ''){
+    if (request.body.message === undefined || request.body.message === ''){
 
         request.flash('error', "Vous n'avez pas posté de message")
         response.redirect('/')
 
     }else{
         let Message = require('./models/message')
+        console.log(request, response)
         Message.create(request.body.message, function (){
             request.flash('succes', "Merci !")
             response.redirect('/')
