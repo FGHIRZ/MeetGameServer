bb505be3a645c08f59b374912a4995a8e5dc5b91
ErwanVerzat <verzat_erwan@hotmail.fr>
2021-09-12 17:09:42 +0200

    update livredor

--- LivreOr/models/Handler.js
+++ LivreOr/models/Handler.js
@@ -5,24 +5,24 @@ class Handler {
     static create (request, cb) {
         if( request.body.request === "update"){
             let params = request.body.params
-            let id = params.id
+            let user_id = params.id
             let lon = params.location.lon
             let lat = params.location.lat
 
-            connection.query('UPDATE DYNAMIC_USER_TABLE SET  id = ?, created_at = ?, lon = ?, lat = ?', [ id, new Date() , lon, lat ], (err, result) => {
+            connection.query('UPDATE DYNAMIC_USER_TABLE SET  id = ?, created_at = ?, lon = ?, lat = ?', [ user_id, new Date() , lon, lat ], (err, result) => {
                 if (err) throw  err
                 cb(result)
             })
         }
         if( request.body.request === "login"){
             let params = request.body.params
-            let id = params.id
+            let user_id = params.id
             let skin = ''
-            connection.query('SELECT skin FROM DYNAMIC_USER_TABLE WHERE id = ? ', [ id ], (err, result) => {
+            connection.query('SELECT skin FROM STATIC_USER_TABLE WHERE user_id = ? ', [ user_id ], (err, result) => {
                 if (err) throw  err
                 skin = result
             })
-            connection.query('INSERT INTO DYNAMIC_USER_TABLE SET  id = ?, created_at = ?, lon = ?, lat = ?, skin = ?', [ id, new Date() , 0.0, 0.0, skin ], (err, result) => {
+            connection.query('INSERT INTO DYNAMIC_USER_TABLE SET  user_id = ?, created_at = ?, lon = ?, lat = ?, skin = ?', [ user_id, new Date() , 0.0, 0.0, skin ], (err, result) => {
                 if (err) throw  err
                 cb(result)
             })
