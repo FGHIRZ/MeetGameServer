b9abe3d365c283b09884e9e1250d362cef342140
COURAU Paul <paul.courau@univ-lorraine.fr>
2021-09-14 15:52:12 +0200

    return insertID

--- LivreOr/models/Handler.js
+++ LivreOr/models/Handler.js
@@ -23,7 +23,7 @@ class Handler {
               var sql = "INSERT INTO STATIC_USER_TABLE (name, skin, password) VALUES (' " + name + "', 'skin1', '1234')"
               connection.query(sql, (err, result) => {
                   if (err) throw  err
-                  console.log(result);
+                  console.log(result.insertedId);
               })
             }
             else{
