7f62f0ae8228067a434ab240b85f7983d5dec810
COURAU Paul <paul.courau@univ-lorraine.fr>
2021-09-17 18:57:10 +0200

    test

--- LivreOr/models/Handler.js
+++ LivreOr/models/Handler.js
@@ -16,8 +16,7 @@ class Handler {
         try {
               let response = await this.check_login_password(name, password)
               let sql_query = "SELECT user_id, name, skin  FROM STATIC_USER_TABLE WHERE name='" + name +"'"
-              let select = await this.query_db(sql_query)
-              let user=select[0]
+              let user = await this.query_db(sql_query)
               console.log("test", user)
               this.update_dynamic_user_table(user.user_id, user.skin)
               response = json_maker.login(user)
@@ -44,26 +43,22 @@ class Handler {
     }
 
     static async check_login_password(name, password){
-      return new Promise(async function(resolve, reject) {
+      return new Promise((resolve, reject) => {
 
         let sql_query = "SELECT name, password FROM STATIC_USER_TABLE WHERE name='" + name +"'"
         let users = await this.query_db(sql_query)
-        let response = ""
 
         if(users.length === 0){
-            response =  json_maker.error(1, "this account does not exist")
+            let response =  json_maker.error(1, "this account does not exist")
             reject(response)
 
         }else{
 
             let user = users[0]
-
             if(user.password === password){
-
-                resolve(response)
-
+                resolve(user)
             }else{
-                response = json_maker.error(2, "the name and the password does not match")
+                let response = json_maker.error(2, "the name and the password does not match")
                 reject(response)
             }
         }
