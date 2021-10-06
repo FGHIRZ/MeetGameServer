cbcc4fc3faf72d6655e0ddc80cf827b75cfa84b7
fghirz <paul.courau@hotmail.fr>
2021-10-06 03:01:09 +0200

    test

--- LivreOr/tokens.js
+++ LivreOr/tokens.js
@@ -20,23 +20,6 @@ class token_manager{
     let test = process.env.TOKEN_SECRET;
     console.log(test)
   }
-
-  static authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-
-  if (token == null) return res.sendStatus(401)
-
-  jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
-    console.log(err)
-
-    if (err) return res.sendStatus(403)
-
-    req.user = user
-
-    next()
-  })
-}
 }
 
 
