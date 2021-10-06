57634762fb30304c81649647fcc2056c8d3e0a44
fghirz <paul.courau@hotmail.fr>
2021-10-06 05:02:00 +0200

    fix

--- LivreOr/models/Handler.js
+++ LivreOr/models/Handler.js
@@ -92,7 +92,7 @@ class Handler {
       console.log("doing info")
       let user_id = params.user_id
       try {
-          let sql_query = "SELECT user_id, user_skin, user_pseudo  FROM STATIC_USER_TABLE WHERE username='" + username +"'"
+          let sql_query = "SELECT user_id, user_skin, user_pseudo  FROM STATIC_USER_TABLE WHERE user_id='" + user_id +"'"
           let select = await this.db_query(sql_query)
           let user = select[0]
           this.update_dynamic_user_table(user.user_id, user.user_skin, user.user_pseudo)
@@ -102,7 +102,7 @@ class Handler {
           cb(e)
       }
     }
-    
+
     static async check_login_password(username, password){
         return new Promise(async (resolve, reject) => {
 
